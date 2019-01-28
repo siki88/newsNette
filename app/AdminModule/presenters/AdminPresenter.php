8bc912a386af272f1a78b16c5e14d976a422e2a4
siki88 <siki@sladkej.cz>
2019-01-28 12:58:56 +0100

    SETTING router

--- app/AdminModule/presenters/AdminPresenter.php
+++ app/AdminModule/presenters/AdminPresenter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Presenters;
+namespace App\AdminModule\Presenters;
 
 use Nette\Application\UI\Presenter,
      Nette\Application\UI\Form,
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 
 //$configurator->setDebugMode(false);
-$configurator->setDebugMode(['46.39.171.47','127.0.0.1','localhost']);
+//$configurator->setDebugMode(['46.39.171.47','127.0.0.1','localhost']);
 $configurator->enableTracy(__DIR__ . '/../log');
 
 $configurator->setTimeZone('Europe/Prague');
